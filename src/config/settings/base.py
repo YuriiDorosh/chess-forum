efcf1ad0a40532310e4eabd0d683eabaf584156d
Yurii <yuriydorosh2005@gmail.com>
2023-09-20 18:03:58 +0300

    UPDATE: base settings(delete debug and allowed hosts)

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -19,12 +19,6 @@ load_dotenv()
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 DJANGO_APPS = [
