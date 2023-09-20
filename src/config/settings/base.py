f83bc04cb399241046a86f302ead9ed19d24bfac
Yurii <yuriydorosh2005@gmail.com>
2023-09-20 17:56:59 +0300

    REFACTOR: black

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -2,6 +2,7 @@ import os
 import sys
 from pathlib import Path
 
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -13,7 +14,6 @@ sys.path.insert(0, os.path.join(BASE_DIR, "apps"))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -36,13 +36,9 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY = [
+THIRD_PARTY = []
 
-]
-
-LOCAL_APPS = [
-
-]
+LOCAL_APPS = []
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY + LOCAL_APPS
 
