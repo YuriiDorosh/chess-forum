7dd1997cb6e745e4ab52f18f38669279d8c5fa95
Yurii <yuriydorosh2005@gmail.com>
2023-09-20 18:05:44 +0300

    REFACTOR: black

--- src/config/settings/prod.py
+++ src/config/settings/prod.py
@@ -4,17 +4,17 @@ from dotenv import load_dotenv
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = [] # Add hosts before deploy
+ALLOWED_HOSTS = []  # Add hosts before deploy
 
 load_dotenv()
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': 5432,
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
