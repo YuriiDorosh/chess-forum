9372e224e1c2cfe1af9a1483667507be0974f00d
Yurii <yuriydorosh2005@gmail.com>
2023-09-20 20:22:47 +0300

    ADD: room_messages app with model Message and tests

---
+++ src/apps/room_messages/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from users.models import User
+from rooms.models import Room
+
+class Message(models.Model):
+    """
+    A model representing messages posted within chat rooms.
+
+    Attributes:
+        room (Room): The chat room where the message is posted.
+        user (User): The user who posted the message.
+        content (str): The content of the message.
+        date_added (datetime): The date and time when the message was added (auto-generated).
+    """
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
---
+++ src/apps/room_messages/tests/test_model.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from datetime import datetime
+from users.models import User
+from rooms.models import Room
+from room_messages.models import Message
+
+class MessageModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username='testuser',
+            bio='Test bio',
+            telegram_id='test123',
+            chess_profile_url='https://example.com/testuser',
+            subscriber=True
+        )
+
+        self.room = Room.objects.create(
+            name='Test Room',
+            slug='test-room',
+            premium=False
+        )
+
+        self.message = Message.objects.create(
+            room=self.room,
+            user=self.user,
+            content='This is a test message.',
+            date_added=datetime.now()
+        )
+
+    def test_message_creation(self):
+        self.assertEqual(self.message.room, self.room)
+        self.assertEqual(self.message.user, self.user)
+        self.assertEqual(self.message.content, 'This is a test message.')
+
+    def test_auto_date_added(self):
+        new_message = Message.objects.create(
+            room=self.room,
+            user=self.user,
+            content='Another test message.'
+        )
+        self.assertIsNotNone(new_message.date_added)
