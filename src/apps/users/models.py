b02d46b5f7862af1eea34502e458479ec55626ea
Yurii <yuriydorosh2005@gmail.com>
2023-09-20 19:39:28 +0300

    ADD: users app with model User

---
+++ src/apps/users/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    """
+    Custom User model that overrides the default Django User model.
+
+    Fields:
+    - bio: User's biography (optional field).
+    - photo: User's photo (stored in the 'user_photos/' folder) (optional field).
+    - telegram_id: Telegram ID (optional field).
+    - chess_profile_url: URL to the user's chess profile (optional field).
+    - subscriber: Whether the user is a subscriber (default is False).
+    """
+    bio = models.TextField(blank=True, null=True)
+    photo = models.ImageField(upload_to='user_photos/', blank=True, null=True)
+    telegram_id = models.CharField(max_length=30, blank=True, null=True)
+    chess_profile_url = models.URLField(max_length=60, blank=True, null=True)
+    subscriber = models.BooleanField(default=False)
---
+++ src/apps/users/tests/test_model.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from users.models import User
+from django.db import IntegrityError
+
+class UserModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username='testuser',
+            bio='Test bio',
+            telegram_id='test123',
+            chess_profile_url='https://example.com/testuser',
+            subscriber=True
+        )
+
+    def test_user_creation(self):
+        self.assertEqual(self.user.username, 'testuser')
+        self.assertEqual(self.user.bio, 'Test bio')
+        self.assertEqual(self.user.telegram_id, 'test123')
+        self.assertEqual(self.user.chess_profile_url, 'https://example.com/testuser')
+        self.assertTrue(self.user.subscriber)
+
+    def test_default_subscriber_value(self):
+        new_user = User.objects.create(username='newuser')
+        self.assertFalse(new_user.subscriber)
+
+    def test_unique_username_constraint(self):
+        with self.assertRaises(IntegrityError):
+            duplicate_user = User(
+                username='testuser',
+                bio='Duplicate user',
+                telegram_id='test456',
+                chess_profile_url='https://example.com/duplicateuser',
+                subscriber=False
+            )
+            duplicate_user.save()
