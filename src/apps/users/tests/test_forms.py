4858d3006fdc98be34e8c8d114a1b684e1bc3fbc
Yurii <yuriydorosh2005@gmail.com>
2023-09-21 19:25:55 +0300

    ADD: tests for registration form

---
+++ src/apps/users/tests/test_forms.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from users.forms import RegistrationForm, LoginForm
+
+from users.models import User
+
+class RegistrationFormTest(TestCase):
+    def test_registration_form_valid_data(self):
+        form_data = {
+            'first_name': 'first name',
+            'last_name': 'last name',
+            'username': 'testuser',
+            'email': 'test@example.com',
+            'password1': 'securepassword',
+            'password2': 'securepassword',
+        }
+        form = RegistrationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        
+
+    def test_registration_form_invalid_data(self):
+        form_data = {
+            'first_name': 'first name',
+            'last_name': 'last name',
+            'username': 'testuser',
+            'email': 'invalid-email',
+            'password1': 'securepassword',
+            'password2': 'securepassword',
+        }
+        form = RegistrationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+
