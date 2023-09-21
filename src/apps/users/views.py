c1d677bbe6fa6c1a87540f296a7f5af8c94a4b9d
Yurii <yuriydorosh2005@gmail.com>
2023-09-21 18:52:47 +0300

    UPDATE: users views(login, registration and logout)

--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
-# Create your views here.
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.views.generic.edit import CreateView
+from users.forms import RegistrationForm, LoginForm
+from users.models import User
+
+
+
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    form_class = LoginForm
+
+
+
+
+class UserRegistrationView(CreateView):
+    model = User
+    form_class = RegistrationForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:home')
+
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')  
