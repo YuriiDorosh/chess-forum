96632ce41e17903f608065a383989cbdaf4f0c39
Yurii <yuriydorosh2005@gmail.com>
2023-09-21 18:51:11 +0300

    ADD: forms for sign in and sign up

---
+++ src/apps/users/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from users.models import User
+
+class RegistrationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': "Enter a name"}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Enter your last name'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': "Enter a username"}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Enter your email address'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Confirm password'}))
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+        
+        
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': "Enter a username"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Password'}))
+    
+    class Meta:
+        model = User
+        fields = ['username', 'password']
