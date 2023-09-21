08b49b959714d8eff3f061be6d6db0641db83c08
Yurii <yuriydorosh2005@gmail.com>
2023-09-21 22:51:58 +0300

    UPDATE: users urls(/profile)

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from users import views
 
@@ -6,5 +7,6 @@ app_name = "users"
 urlpatterns = [
     path("registration/", views.UserRegistrationView.as_view(), name="registration"),
     path("login/", views.UserLoginView.as_view(), name="login"),
+    path("profile/<int:pk>", login_required(views.UserProfileView.as_view()), name='profile'),
     path("logout/", views.user_logout, name="logout"),
 ]
