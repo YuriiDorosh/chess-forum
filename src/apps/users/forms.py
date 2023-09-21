84bc04f8d787b3a35fa1325b0d4a577b8d6cf923
Yurii <yuriydorosh2005@gmail.com>
2023-09-21 22:50:52 +0300

    UPDATE: users forms(UserProfileForm)

--- src/apps/users/forms.py
+++ src/apps/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
 
 from users.models import User
 
@@ -40,3 +40,14 @@ class LoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ["username", "password"]
+        
+class UserProfileForm(UserChangeForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'readonly': True}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'readonly': True}))
+    class Meta:
+        model = User
+
+        fields = ('first_name', 'last_name', 'image', 'username', 'email')
