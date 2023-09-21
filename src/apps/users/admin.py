c81b0a0ac2cafb369038ebab5698e913b7bc160d
Yurii <yuriydorosh2005@gmail.com>
2023-09-21 18:50:21 +0300

    UPDATE: register User model

--- src/apps/users/admin.py
+++ src/apps/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(User, UserAdmin)
