a2d3a730b6c58e36db64c2e29cc635866aa845ed
Yurii <yuriydorosh2005@gmail.com>
2023-09-22 00:43:46 +0300

    UPDATE: profile/edit url

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("registration/", views.UserRegistrationView.as_view(), name="registration"),
     path("login/", views.UserLoginView.as_view(), name="login"),
     path("profile/<int:pk>", login_required(views.UserProfileView.as_view()), name="profile"),
+    path("profile/edit/<int:pk>/", login_required(views.UserProfileEditView.as_view()), name='profile_edit'),
     path("logout/", views.user_logout, name="logout"),
 ]
