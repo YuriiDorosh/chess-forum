ec916730e8b1aaf99eb95c4474b2ae74943892d8
Yurii <yuriydorosh2005@gmail.com>
2023-09-21 18:49:35 +0300

    UPDATE: base settings(AUTHENTICATION_BACKENDS)

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -123,3 +123,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
