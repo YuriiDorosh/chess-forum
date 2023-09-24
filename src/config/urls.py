12477420260866ca0635770dda28b8e811407b3f
Yurii <yuriydorosh2005@gmail.com>
2023-09-25 00:25:36 +0300

    UPDATE: home url(all posts)

--- src/config/urls.py
+++ src/config/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from apps.posts.views import all_posts_views
 
 urlpatterns = [
+    path("", all_posts_views.all_user_posts, name="home"),
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("posts/", include("posts.urls")),
