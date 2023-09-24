85582b0b95996b7d35f9e1fb9ad9315155e69fb9
Yurii <yuriydorosh2005@gmail.com>
2023-09-24 18:17:18 +0300

    UPDATE: main urls(posts)

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,7 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [path("admin/", admin.site.urls), path("users/", include("users.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls), 
+    path("users/", include("users.urls")),
+    path('posts/', include('posts.urls')),
+    ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
