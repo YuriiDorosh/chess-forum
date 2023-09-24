2668df33e7d05e43b73eb1ce8994092b623a5d0a
Yurii <yuriydorosh2005@gmail.com>
2023-09-25 00:26:01 +0300

    UPDATE: posts ulrs(create and edit)

--- src/apps/posts/urls.py
+++ src/apps/posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from posts.views import all_posts_views
+from posts.views import all_posts_views, action_on_the_post_views
 
 app_name = "posts"
 
 urlpatterns = [
     path("", all_posts_views.all_user_posts, name="all_user_posts"),
+    path("create/", action_on_the_post_views.CreateUserPostView.as_view(), name="create_post"),
+    path("edit/<int:post_id>/", action_on_the_post_views.EditUserPostView.as_view(), name="edit_post"),
 ]
