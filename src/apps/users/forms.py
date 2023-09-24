ded3739143a7950563d3bed4809ecba16050d4cb
Yurii <yuriydorosh2005@gmail.com>
2023-09-24 19:26:19 +0300

    FIX: useless image field

--- src/apps/users/forms.py
+++ src/apps/users/forms.py
@@ -45,7 +45,7 @@ class LoginForm(AuthenticationForm):
 class UserProfileForm(UserChangeForm):
     first_name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control py-4"}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control py-4"}))
-    image = forms.ImageField(widget=forms.FileInput(attrs={"class": "custom-file-input"}), required=False)
+    # image = forms.ImageField(widget=forms.FileInput(attrs={"class": "custom-file-input"}), required=False)
     username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control py-4", "readonly": True}))
     email = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control py-4", "readonly": True}))
 
