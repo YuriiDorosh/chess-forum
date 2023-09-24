ceb1bc05a5500e19fcbc62dbad6d1bb5a36dc377
Yurii <yuriydorosh2005@gmail.com>
2023-09-24 18:20:58 +0300

    REFACTOR: black

--- src/apps/posts/views/all_posts_views.py
+++ src/apps/posts/views/all_posts_views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from posts.models import UserPost
 
+
 def all_user_posts(request):
-    posts = UserPost.objects.all().order_by('-date_added')
-    return render(request, 'posts/all_user_posts.html', {'posts': posts})
+    posts = UserPost.objects.all().order_by("-date_added")
+    return render(request, "posts/all_user_posts.html", {"posts": posts})
--- src/config/urls.py
+++ src/config/urls.py
@@ -4,10 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls), 
+    path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
-    path('posts/', include('posts.urls')),
-    ]
+    path("posts/", include("posts.urls")),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
