6e9718cbc7068256bbc71a5f27eae2456b7ea7af
Yurii <yuriydorosh2005@gmail.com>
2023-09-24 16:34:52 +0300

    UPDATE: Post model(__str__)

--- src/apps/posts/models.py
+++ src/apps/posts/models.py
@@ -19,3 +19,7 @@ class UserPost(models.Model):
     game_link = models.URLField(max_length=60, blank=True, null=True)
     body = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self) -> str:
+        return f"User: {self.user} | Post: {self.title}"
