3db0c7bf168287f93afc3d9faf1423518b1f4551
Yurii <yuriydorosh2005@gmail.com>
2023-09-25 00:25:19 +0300

    ADD: views file with action on the post(CreateUserPostView, EditUserPostView)

---
+++ src/apps/posts/views/action_on_the_post_views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect, get_object_or_404, Http404
+from django.contrib.auth.decorators import login_required
+from django.views import View
+from posts.models import UserPost
+from posts.forms import UserPostCreateForm, UserPostUpdateForm
+
+
+class CreateUserPostView(View):
+    template_name = "posts/create_post.html"
+
+    def get(self, request):
+        form = UserPostCreateForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = UserPostCreateForm(request.POST)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.user = request.user
+            new_post.save()
+            return redirect("posts:all_user_posts")  
+
+
+
+class EditUserPostView(View):
+    template_name = "posts/edit_post.html"
+
+    def get(self, request, post_id):
+        post = get_object_or_404(UserPost, id=post_id)
+        
+        if post.user != request.user:
+            raise Http404("Post not found or you don't have permission to edit it")
+
+        form = UserPostUpdateForm(instance=post)
+        return render(request, self.template_name, {'form': form, 'post': post})
+
+    def post(self, request, post_id):
+        post = get_object_or_404(UserPost, id=post_id)
+
+        if post.user != request.user:
+            raise Http404("Post not found or you don't have permission to edit it")
+
+        form = UserPostUpdateForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect("posts:all_user_posts")  
