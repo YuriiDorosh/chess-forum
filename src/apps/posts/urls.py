2f67a794d94a79a14d8c849a5f5cb08ca3e6b0a6
Yurii <yuriydorosh2005@gmail.com>
2023-09-24 19:26:08 +0300

    UPDATE: posts/all -> posts/

--- src/apps/posts/urls.py
+++ src/apps/posts/urls.py
@@ -4,5 +4,5 @@ from posts.views import all_posts_views
 app_name = "posts"
 
 urlpatterns = [
-    path("all/", all_posts_views.all_user_posts, name="all_user_posts"),
+    path("", all_posts_views.all_user_posts, name="all_user_posts"),
 ]
