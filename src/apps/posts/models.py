dadc6fe44c9b427407b7e90d4a684d64a6819d68
Yurii <yuriydorosh2005@gmail.com>
2023-09-26 11:57:35 +0300

    ADD: Like model and update UserPost model(new field: likes)

--- src/apps/posts/models.py
+++ src/apps/posts/models.py
@@ -11,6 +11,7 @@ class UserPost(models.Model):
         title (CharField): The title of the post (limited to 100 characters).
         game_link (URLField, optional): A link to a chess game related to the post (optional).
         body (TextField): The main content of the post.
+        likes (ManyToManyField): likes of post.
         date_added (DateTimeField): The date and time when the post was created.
     """
 
@@ -18,7 +19,19 @@ class UserPost(models.Model):
     title = models.CharField(max_length=100)
     game_link = models.URLField(max_length=60, blank=True, null=True)
     body = models.TextField()
+    likes = models.ManyToManyField(User, through='Like', related_name='liked_posts')
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return f"User: {self.user} | Post: {self.title} | ID : {self.id}"
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(UserPost, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['user', 'post']
+        
+    def __str__(self) -> str:
+        return f"Who liked: {self.user} | Post Creator: {self.post.user} | Post: {self.post.title}"
