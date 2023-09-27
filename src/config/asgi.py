50583dfbd2a93ed7afd8278c7c0517f02aac5067
Yurii <yuriydorosh2005@gmail.com>
2023-09-27 15:14:43 +0300

    UPDATE: asgi(websocket)

--- src/config/asgi.py
+++ src/config/asgi.py
@@ -1,16 +1,15 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
 
-application = get_asgi_application()
+django_application = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from apps.rooms import routing
+
+application = ProtocolTypeRouter({
+    "http": django_application,
+    "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+})
