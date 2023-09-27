0fe040e2a7ce25bb56ed1432f9803e8e508a6c0d
Yurii <yuriydorosh2005@gmail.com>
2023-09-27 15:19:08 +0300

    UPDATE: rooms views(room, rooms)

--- src/apps/rooms/views.py
+++ src/apps/rooms/views.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from rooms.models import Room
+from room_messages.models import Message
+
+
+@login_required
+def rooms(request):
+    rooms = Room.objects.all()
+    
+    return render(request, "room/rooms.html", {"rooms": rooms})
+  
+  
+@login_required
+def room(request, slug):
+    room = Room.objects.get(slug=slug)
+    messages = Message.objects.filter(room=room)[0:25]
+
+    return render(request, 'room/room.html', {'room': room, 'messages': messages})
+      
