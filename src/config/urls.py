45129ced1b41b36c938b1240004de84e833d3bbf
Yurii <yuriydorosh2005@gmail.com>
2023-09-27 15:15:12 +0300

    UPDATE: main urls(rooms)

--- src/config/urls.py
+++ src/config/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("posts/", include("posts.urls")),
+    path("rooms/", include("rooms.urls")),
 ]
 
 if settings.DEBUG:
