f8d315a14be812dbbc4f5705fd3e138f5ea07c9d
Yurii <yuriydorosh2005@gmail.com>
2023-10-01 19:42:57 +0300

    UPDATE: posts urls(int -> uuid)

--- src/apps/posts/urls.py
+++ src/apps/posts/urls.py
@@ -7,6 +7,6 @@ app_name = "posts"
 urlpatterns = [
     path("", all_posts_views.all_user_posts, name="all_user_posts"),
     path("create/", action_on_the_post_views.CreateUserPostView.as_view(), name="create_post"),
-    path("edit/<int:post_id>/", action_on_the_post_views.EditUserPostView.as_view(), name="edit_post"),
-    path("like/<int:post_id>/", like_posts_views.LikePostView.as_view(), name="like_post"),
+    path("edit/<uuid:post_id>/", action_on_the_post_views.EditUserPostView.as_view(), name="edit_post"),
+    path("like/<uuid:post_id>/", like_posts_views.LikePostView.as_view(), name="like_post"),
 ]
