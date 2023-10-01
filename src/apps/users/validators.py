55bb9ea3bcad907d980622b90daad4b406d463ee
Yurii <yuriydorosh2005@gmail.com>
2023-10-02 00:30:02 +0300

    ADD: chess profile url validator

---
+++ src/apps/users/validators.py
@@ -0,0 +1,5 @@
+from django.core.exceptions import ValidationError
+
+def validate_chess_profile_url(value):
+    if not value.startswith("https://www.chess.com/member/"):
+        raise ValidationError("Invalid chess profile URL. It should start with 'https://www.chess.com/member/'.")
