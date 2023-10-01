cd0b9d8bd7c12524efe8b9286e5a19f965666d92
Yurii <yuriydorosh2005@gmail.com>
2023-10-01 19:42:21 +0300

    UPDATE: users urls(int -> uuid)

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -7,7 +7,7 @@ app_name = "users"
 urlpatterns = [
     path("registration/", authentication_views.UserRegistrationView.as_view(), name="registration"),
     path("login/", authentication_views.UserLoginView.as_view(), name="login"),
-    path("profile/<int:pk>", login_required(profile_views.UserProfileView.as_view()), name="profile"),
-    path("profile/edit/<int:pk>/", login_required(profile_views.UserProfileEditView.as_view()), name="profile_edit"),
+    path("profile/<uuid:pk>", login_required(profile_views.UserProfileView.as_view()), name="profile"),
+    path("profile/edit/<uuid:pk>/", login_required(profile_views.UserProfileEditView.as_view()), name="profile_edit"),
     path("logout/", authentication_views.user_logout, name="logout"),
 ]
