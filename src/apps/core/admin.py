4f12b7ad92e584ffe2dec32d0feb6ec726e99939
Yurii <yuriydorosh2005@gmail.com>
2023-10-02 20:11:28 +0300

    UPDATE: main admin panel(Discussion, Reply, ReplyLke)

--- src/apps/core/admin.py
+++ src/apps/core/admin.py
@@ -4,7 +4,7 @@ from users.models import User
 from posts.models import UserPost, Like
 from rooms.models import Room
 from room_messages.models import Message
-
+from discussions.models import Discussion, Reply, ReplyLike
 
 class UserAdmin(admin.ModelAdmin):
     pass
@@ -21,13 +21,23 @@ class LikeAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
     pass
 
-
 class MessageAdmin(admin.ModelAdmin):
     pass
 
+class DiscussionAdmin(admin.ModelAdmin):
+    pass
+
+class ReplyAdmin(admin.ModelAdmin):
+    pass
+
+class ReplyLikeAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(User, UserAdmin)
 admin.site.register(UserPost, UserPostAdmin)
 admin.site.register(Like, LikeAdmin)
 admin.site.register(Room, RoomAdmin)
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Discussion, DiscussionAdmin)
+admin.site.register(Reply, ReplyAdmin)
+admin.site.register(ReplyLike, ReplyLikeAdmin)
