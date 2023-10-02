53c34d038cd81f0990c02627a3dd8917d8b83cba
Yurii <yuriydorosh2005@gmail.com>
2023-10-02 20:14:21 +0300

    ADD: discussions app

---
+++ src/apps/discussions/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+from discussions import views
+
+app_name = "discussions"
+
+urlpatterns = [
+    path('', views.discussion_list, name='discussion_list'),
+    # path('discussion/<uuid:discussion_id>/', views.discussion_detail, name='discussion_detail'),
+    path('<uuid:discussion_id>/', views.discussion_detail, name='discussion_detail'),
+    path('create/', views.create_discussion, name='create_discussion'),
+    path('closed/', views.closed_discussions, name='closed_discussions'),
+]
