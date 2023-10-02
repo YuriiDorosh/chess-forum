2ec14b04b43f29336258eea9a19f5f972eb9cdd7
Yurii <yuriydorosh2005@gmail.com>
2023-10-02 23:40:07 +0300

    UPDATE: profile view(context[user_discussions])

--- src/apps/users/views/profile_views.py
+++ src/apps/users/views/profile_views.py
@@ -6,7 +6,7 @@ from django.http import Http404
 from users.forms import UserProfileForm
 from users.models import User
 from posts.models import UserPost
-
+from discussions.models import Discussion 
 
 class UserProfileView(UpdateView):
     model = User
@@ -21,8 +21,10 @@ class UserProfileView(UpdateView):
         context = super().get_context_data(**kwargs)
         user_posts = UserPost.objects.filter(user=self.object)
         liked_posts = UserPost.objects.filter(likes__in=[self.object])
+        user_discussions = Discussion.objects.filter(author=self.object)
         context["user_posts"] = user_posts
         context["liked_posts"] = liked_posts  
+        context["user_discussions"] = user_discussions
         return context
 
 
