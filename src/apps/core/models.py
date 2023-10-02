951c5fda60601875f2d2ac9bbf4e83e92da2a407
Yurii <yuriydorosh2005@gmail.com>
2023-10-02 20:11:49 +0300

    UPDATE: core models(BaseImage)

--- src/apps/core/models.py
+++ src/apps/core/models.py
@@ -2,6 +2,7 @@ import uuid
 
 from django.db import models
 
+from versatileimagefield.fields import VersatileImageField
 
 
 class BaseModel(models.Model):
@@ -14,3 +15,23 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
         ordering = ('-date_added',)
+        
+class BaseImage(models.Model):
+    """Basic model for images"""
+
+    title = models.CharField(max_length=200, null=True, blank=True)
+    alt = models.CharField(max_length=200, null=True, blank=True)
+    image = VersatileImageField(null=True, blank=True, upload_to="images")
+
+    class Meta:
+        abstract = True
+        verbose_name = "Image"
+        verbose_name_plural = "Images"
+
+    def __str__(self):
+        res = ""
+        if self.title:
+            res = self.title
+        else:
+            res = self.image.url
+        return res
