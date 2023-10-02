3de3d8096786dea681418b7178d55edf508d9ddc
Yurii <yuriydorosh2005@gmail.com>
2023-10-03 00:02:24 +0300

    ADD: core/views with home view

---
+++ src/apps/core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.db.models import Count
+from posts.models import UserPost
+from discussions.models import Discussion
+
+def home(request):
+
+    top_posts = UserPost.objects.annotate(like_count=Count('likes')).order_by('-like_count')[:10]
+
+
+    open_discussions = Discussion.objects.filter(closed=False)
+
+    context = {
+        'top_posts': top_posts,
+        'open_discussions': open_discussions,
+    }
+
+    return render(request, 'core/home.html', context)
