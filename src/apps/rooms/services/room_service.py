46d763b9bb7c5f70f65fbf3514c3df4a98019aa9
Yurii <yuriydorosh2005@gmail.com>
2023-10-03 15:54:17 +0300

    UPDATE: rooms/services/room_service(documentations)

--- src/apps/rooms/services/room_service.py
+++ src/apps/rooms/services/room_service.py
@@ -1,11 +1,25 @@
 from rooms.models.room import Room
 
-
 class RoomService:
     @staticmethod
     def get_all_rooms():
+        """
+        Retrieves all rooms from the database.
+
+        Returns:
+            QuerySet: A query containing all rooms.
+        """
         return Room.objects.all()
 
     @staticmethod
     def get_room_by_slug(some_slug):
+        """
+        Retrieves a room from the database by its slug.
+
+        Args:
+            some_slug (str): The slug of the room to retrieve.
+
+        Returns:
+            Room: The room with the specified slug.
+        """
         return Room.objects.get(slug=some_slug)
