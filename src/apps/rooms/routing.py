df9831e4f66588c304a06057c6afdd365f3118eb
Yurii <yuriydorosh2005@gmail.com>
2023-10-03 15:45:25 +0300

    UPDATE: rooms/routing(delete comment)

--- src/apps/rooms/routing.py
+++ src/apps/rooms/routing.py
@@ -3,5 +3,4 @@ from rooms.consumers import ChatConsumer
 
 websocket_urlpatterns = [
     path("ws/<str:room_name>/", ChatConsumer.as_asgi()),
-    # path('<str:room_name>/', ChatConsumer.as_asgi()),
 ]
