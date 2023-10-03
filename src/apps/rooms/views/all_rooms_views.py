f22b2f930439e7d12e08e3c45fe0ff70241447e2
Yurii <yuriydorosh2005@gmail.com>
2023-10-03 15:43:22 +0300

    REFACTOR: rooms/views.py -> rooms/views/

---
+++ src/apps/rooms/views/all_rooms_views.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.views import View
+from django.utils.decorators import method_decorator
+from rooms.services.room_service import RoomService  
+
+@method_decorator(login_required, name='dispatch')
+class AllRoomsView(View):
+    """
+    View for displaying a list of all available rooms.
+
+    Attributes:
+        None
+
+    Methods:
+        get(self, request): Handles GET requests and renders the list of rooms.
+
+    Template:
+        room/rooms.html
+    """
+
+    def get(self, request):
+        """
+        Handles GET requests and renders the list of all available rooms.
+
+        Retrieves a list of all rooms using the RoomService.
+
+        Args:
+            request (HttpRequest): The HTTP GET request object.
+
+        Returns:
+            HttpResponse: The rendered list of rooms.
+        """
+        rooms = RoomService.get_all_rooms() 
+        return render(request, "room/rooms.html", {"rooms": rooms})
---
+++ src/apps/rooms/views/room_views.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.views import View
+from django.utils.decorators import method_decorator
+from rooms.services.room_service import RoomService  
+from room_messages.models import Message
+
+@method_decorator(login_required, name='dispatch')
+class RoomView(View):
+    """
+    View for displaying a room's details.
+
+    Attributes:
+        None
+
+    Methods:
+        get(self, request, slug): Handles GET requests and renders the room details page.
+
+    Template:
+        room/room.html
+    """
+
+    def get(self, request, slug):
+        """
+        Handles GET requests and renders the room details page.
+
+        Retrieves a room by its slug and retrieves the latest 25 messages for the room.
+
+        Args:
+            request (HttpRequest): The HTTP GET request object.
+            slug (str): The slug of the room to display.
+
+        Returns:
+            HttpResponse: The rendered room details page.
+        """
+        room = RoomService.get_room_by_slug(slug)
+        messages = Message.objects.filter(room=room)[0:25]
+        return render(request, "room/room.html", {"room": room, "messages": messages})
