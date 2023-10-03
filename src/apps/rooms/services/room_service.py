3c1604f5314c81f19aec56f5051503f2538e6c9e
Yurii <yuriydorosh2005@gmail.com>
2023-10-03 15:43:35 +0300

    ADD: room service

---
+++ src/apps/rooms/services/room_service.py
@@ -0,0 +1,10 @@
+from rooms.models.room import Room
+
+class RoomService:
+    @staticmethod
+    def get_all_rooms():
+        return Room.objects.all()
+    
+    @staticmethod
+    def get_room_by_slug(some_slug):
+        return Room.objects.get(slug=some_slug)
