4fa804452033782b4378e4789ff9a1219077451d
Yurii <yuriydorosh2005@gmail.com>
2023-10-04 18:41:28 +0300

    REFACTOR: black

--- src/apps/core/admin.py
+++ src/apps/core/admin.py
@@ -9,44 +9,52 @@ from room_messages.models.message import Message
 from rooms.models.room import Room
 from users.models.user import User
 
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'subscriber')
-    list_filter = ('subscriber',)
+    list_display = ("username", "subscriber")
+    list_filter = ("subscriber",)
+
 
 @admin.register(UserPost)
 class UserPostAdmin(admin.ModelAdmin):
-    list_display = ('user', 'title', 'date_added')
-    list_filter = ('user', 'date_added')
-    search_fields = ('title', 'body')
+    list_display = ("user", "title", "date_added")
+    list_filter = ("user", "date_added")
+    search_fields = ("title", "body")
+
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
-    list_display = ('user', 'post', 'date_added')
-    list_filter = ('user', 'date_added')
+    list_display = ("user", "post", "date_added")
+    list_filter = ("user", "date_added")
+
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', 'premium')
-    list_filter = ('premium',)
+    list_display = ("name", "slug", "premium")
+    list_filter = ("premium",)
+
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('room', 'user', 'date_added')
-    list_filter = ('room', 'user', 'date_added')
+    list_display = ("room", "user", "date_added")
+    list_filter = ("room", "user", "date_added")
+
 
 @admin.register(Discussion)
 class DiscussionAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'closed')
-    list_filter = ('author', 'closed')
-    search_fields = ('title', 'text')
+    list_display = ("title", "author", "closed")
+    list_filter = ("author", "closed")
+    search_fields = ("title", "text")
+
 
 @admin.register(ReplyLike)
 class ReplyLikeAdmin(admin.ModelAdmin):
-    list_display = ('user', 'reply')
-    list_filter = ('user', 'reply')
+    list_display = ("user", "reply")
+    list_filter = ("user", "reply")
+
 
 @admin.register(Reply)
 class ReplyAdmin(admin.ModelAdmin):
-    list_display = ('discussion', 'author', 'date_added')
-    list_filter = ('discussion', 'author', 'date_added')
+    list_display = ("discussion", "author", "date_added")
+    list_filter = ("discussion", "author", "date_added")
