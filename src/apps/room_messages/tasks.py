432036a07a563c6ea583a72b5e4fd026ae669dda
Yurii <yuriydorosh2005@gmail.com>
2023-10-05 18:17:15 +0300

    ADD: room_messages/tasks

---
+++ src/apps/room_messages/tasks.py
@@ -0,0 +1,10 @@
+from celery import shared_task
+from datetime import timedelta
+from django.utils import timezone
+from room_messages.models.message import Message
+
+@shared_task
+def delete_old_messages():
+    one_month_ago = timezone.now() - timedelta(days=30)
+
+    Message.objects.filter(date_added__lt=one_month_ago).delete()
