1d2dda485afaa78b70e8b42c5dbbc7f6ea4c4e6f
Yurii <yuriydorosh2005@gmail.com>
2023-10-05 20:09:12 +0300

    UPDATE: base settings(redis)

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -139,8 +139,10 @@ AUTHENTICATION_BACKENDS = [
 LOGIN_URL = "users:login"
 LOGIN_REDIRECT_URL = "/users/profile/"
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'  
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'  
+# CELERY_BROKER_URL = 'redis://localhost:6379/0'  
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'  
+CELERY_BROKER_URL = 'redis://redis:6379/0'  
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'  
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
