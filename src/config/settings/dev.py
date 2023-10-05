bbbd1b3b7f5fc8484479e6fc60cf128c6c6d15f9
Yurii <yuriydorosh2005@gmail.com>
2023-10-05 20:09:27 +0300

    UPDATE: dev settings(PostgreSQL)

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -5,8 +5,12 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
