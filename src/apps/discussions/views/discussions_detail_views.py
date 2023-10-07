56c098d9b662ef6d220f286d875e1d24fe3af07e
Yurii <yuriydorosh2005@gmail.com>
2023-10-07 19:26:48 +0300

    UPDATE: discussions detail view

--- src/apps/discussions/views/discussions_detail_views.py
+++ src/apps/discussions/views/discussions_detail_views.py
@@ -4,7 +4,7 @@ from discussions.services.reply_like_service import ReplyLikeService
 from discussions.services.reply_service import ReplyService
 from django.shortcuts import redirect, render
 from django.views import View
-
+from django.urls import reverse_lazy
 
 class DiscussionListView(View):
     """
@@ -32,39 +32,51 @@ class DiscussionDetailView(View):
     Processes the form data to create a reply, toggle a reply like, or close the discussion (if authorized).
     """
 
+
+
     def get(self, request, discussion_id):
         discussion = DiscussionService.get_discussion_by_id(discussion_id)
         replies = ReplyService.get_replies_for_discussion(discussion)
-        reply_form = ReplyForm()
+        initial_data = {"discussion_id": discussion_id}  
+        reply_form = ReplyForm(initial=initial_data)  
         return render(
-            request,
-            "discussions/discussion_detail.html",
-            {"discussion": discussion, "replies": replies, "reply_form": reply_form},
-        )
+        request,
+        "discussions/discussion_detail.html",
+        {"discussion": discussion, "replies": replies, "reply_form": reply_form},
+    )
 
+
+    
     def post(self, request, discussion_id):
         discussion = DiscussionService.get_discussion_by_id(discussion_id)
         replies = ReplyService.get_replies_for_discussion(discussion)
-        reply_form = ReplyForm()
+
+
 
         if request.method == "POST":
+            if "close_discussion" in request.POST and request.user == discussion.author:
+                DiscussionService.close_discussion(discussion)
+                return redirect("discussions:discussion_list")
+
+        
             reply_form = ReplyForm(request.POST, request.FILES)
             if reply_form.is_valid():
                 ReplyService.create_reply(
                     discussion, request.user, reply_form.cleaned_data
-                )
-                return redirect("discussion_detail", discussion_id=discussion_id)
+            )
+                return redirect("discussions:discussion_detail", discussion_id=discussion_id)
 
             if "like_reply" in request.POST:
                 reply_id = request.POST["like_reply"]
                 reply_to_like = ReplyService.get_reply_by_id(reply_id)
                 ReplyLikeService.toggle_like(request.user, reply_to_like)
 
-            if "close_discussion" in request.POST and request.user == discussion.author:
-                DiscussionService.close_discussion(discussion)
+        else:
+            reply_form = ReplyForm()  # Створюємо пусту форму для відображення
 
         return render(
-            request,
-            "discussions/discussion_detail.html",
-            {"discussion": discussion, "replies": replies, "reply_form": reply_form},
-        )
+        request,
+        "discussions/discussion_detail.html",
+        {"discussion": discussion, "replies": replies, "reply_form": reply_form},
+    )
+
