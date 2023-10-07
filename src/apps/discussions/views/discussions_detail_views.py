79de126f93aa7e2f0eb575162f8e52259898ed8b
Yurii <yuriydorosh2005@gmail.com>
2023-10-08 02:10:02 +0300

    UPDATE: discussion detail view(use reply like form)

--- src/apps/discussions/views/discussions_detail_views.py
+++ src/apps/discussions/views/discussions_detail_views.py
@@ -1,10 +1,11 @@
 from discussions.forms.reply_forms import ReplyForm
+from discussions.forms.reply_like_forms import ReplyLikeForm
 from discussions.services.discussion_service import DiscussionService
 from discussions.services.reply_like_service import ReplyLikeService
 from discussions.services.reply_service import ReplyService
 from django.shortcuts import redirect, render
 from django.views import View
-from django.urls import reverse_lazy
+
 
 
 class DiscussionListView(View):
@@ -62,16 +63,23 @@ class DiscussionDetailView(View):
                     "discussions:discussion_detail", discussion_id=discussion_id
                 )
 
-            if "like_reply" in request.POST:
-                reply_id = request.POST["like_reply"]
+            like_form = ReplyLikeForm(request.POST)  
+            if like_form.is_valid():
+                reply_id = like_form.cleaned_data["reply_id"]
                 reply_to_like = ReplyService.get_reply_by_id(reply_id)
                 ReplyLikeService.toggle_like(request.user, reply_to_like)
 
         else:
             reply_form = ReplyForm()
+            like_form = ReplyLikeForm()
 
         return render(
             request,
             "discussions/discussion_detail.html",
-            {"discussion": discussion, "replies": replies, "reply_form": reply_form},
+            {
+                "discussion": discussion,
+                "replies": replies,
+                "reply_form": reply_form,
+                "like_form": like_form,
+            },
         )
