4eb7afc5b6a2378775e3621ffec1dcfaa7338205
Yurii <yuriydorosh2005@gmail.com>
2023-10-08 18:45:30 +0300

    UPDATE: admin panel(image to post)

--- src/apps/core/admin.py
+++ src/apps/core/admin.py
@@ -3,6 +3,7 @@ from discussions.models.reply import Reply
 from discussions.models.reply_like import ReplyLike
 from django.contrib import admin
 from posts.models.post import UserPost
+from posts.models.post import UserPostImage
 from posts.models.post_likes import Like
 from room_messages.models.message import Message
 from rooms.models.room import Room
@@ -20,6 +21,10 @@ class UserPostAdmin(admin.ModelAdmin):
     list_display = ("user", "title", "date_added")
     list_filter = ("user", "date_added")
     search_fields = ("title", "body")
+
+@admin.register(UserPostImage)
+class UserPostImageAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Like)
