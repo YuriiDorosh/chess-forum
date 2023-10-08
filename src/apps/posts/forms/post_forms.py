f6d3e8f118784357159a7e7ff705179b581b9825
Yurii <yuriydorosh2005@gmail.com>
2023-10-08 18:47:47 +0300

    UPDATE: post fomr(image)

--- src/apps/posts/forms/post_forms.py
+++ src/apps/posts/forms/post_forms.py
@@ -1,14 +1,15 @@
 from django import forms
 from posts.models.post import UserPost
 
-
 class UserPostCreateForm(forms.ModelForm):
+    image = forms.ImageField(required=False)  
     class Meta:
         model = UserPost
-        fields = ["title", "game_link", "body"]
-
+        fields = ["title", "game_link", "body", "image"] 
 
 class UserPostUpdateForm(forms.ModelForm):
+    image = forms.ImageField(required=False) 
+
     class Meta:
         model = UserPost
-        fields = ["title", "game_link", "body"]
+        fields = ["title", "game_link", "body", "image"]  
